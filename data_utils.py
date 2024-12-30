ec06c1dc6edbfe10069b455497eaf34b4e92112f
MyeongheonChoi <audgjs99@gmail.com>
2024-12-30 16:04:55 +0900

    Add sft files

---
+++ data_utils.py
@@ -0,0 +1,159 @@
+# adapted from https://github.com/huggingface/trl/blob/main/examples/scripts/dpo.py#L82
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+from datasets import load_dataset, load_from_disk
+from abc import ABC, abstractmethod
+
+from datasets import Dataset, concatenate_datasets
+
+
+DEFAULT_PROMPT_TEMPLATE = "\n\nHuman:\n{raw_prompt}\n\nAssistant:\n"
+
+@dataclass
+class RawDatasetPreprocessor(ABC):
+    path: Optional[str] = None
+    prompt_template: Optional[str] = DEFAULT_PROMPT_TEMPLATE
+    sanity_check: Optional[bool] = False
+    num_proc: Optional[int] = 4
+
+    @abstractmethod
+    def _get_raw_dataset(self, split) -> Dataset:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _dataset_to_preference_formatter(self, example) -> Dict[str, str]:
+        # return {
+        #     "raw_prompt": str, # optional, useful for generation
+        #     "prompt":   str,
+        #     "chosen":   str,
+        #     "rejected": str,
+        # }
+        raise NotImplementedError
+
+    def get_preference_dataset(self, split) -> Dataset:
+        """
+        return a dataset of texts with three keys "prompt", "chosen", "rejected", ("raw_prompt", optional)
+        """
+        dataset = self._get_raw_dataset(split)
+        if self.sanity_check: dataset = dataset.select(range(min(len(dataset), 100)))
+        print("mapping dataset to standard format...")
+        return dataset.map(self._dataset_to_preference_formatter, num_proc=self.num_proc, remove_columns=dataset.column_names)
+
+    def get_sft_dataset(self, split, chosen_only=True):
+        """
+        return a dataset of texts with two keys "prompt", "response", ("raw_prompt", optional)
+        """
+        print("mapping preference to sft...")
+        dataset = self.get_preference_dataset(split)
+        # chosen_only_dataset = dataset.rename_column("chosen", "response")
+        chosen_only_dataset = dataset.remove_columns("rejected").rename_column("chosen", "response")
+        if chosen_only:
+            return chosen_only_dataset
+        rejected_only_dataset = dataset.remove_columns("chosen").rename_column("rejected", "response")
+        return concatenate_datasets([chosen_only_dataset, rejected_only_dataset])
+
+
+def preprocess_anthropic_prompt_and_response(prompt_and_response):
+    prompt_and_response = prompt_and_response.replace("\n\nHuman: ", "\n\nHuman:\n")
+    prompt_and_response = prompt_and_response.replace("\n\nAssistant: ", "\n\nAssistant:\n")
+    return prompt_and_response
+
+def extract_anthropic_prompt_and_response(prompt_and_response):
+    """Extract the anthropic prompt from a prompt and response pair."""
+    search_term = "\n\nAssistant:\n"
+    search_term_idx = prompt_and_response.rfind(search_term)
+    assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
+    return prompt_and_response[: search_term_idx + len(search_term)]
+
+
+@dataclass
+class HhRlhfRDP(RawDatasetPreprocessor):
+    path: Optional[str] = "Anthropic/hh-rlhf"
+
+    # def __post_init__(self):
+    #     assert self.prompt_template == "\n\nHuman: {prompt}\n\nAssistant:"
+
+    def _get_raw_dataset(self, split):
+        if split == "train":
+            return load_dataset(self.path, split="train").train_test_split(test_size=0.1, seed=0)["train"]
+        elif split == "validation":
+            return load_dataset(self.path, split="train").train_test_split(test_size=0.1, seed=0)["test"]
+        elif split == "test":
+            return load_dataset(self.path, split="test")
+        else:
+            raise NotImplementedError
+
+    def _dataset_to_preference_formatter(self, example) -> Dict[str, str]:
+        example["chosen"]   = preprocess_anthropic_prompt_and_response(example["chosen"])
+        example["rejected"] = preprocess_anthropic_prompt_and_response(example["rejected"])
+        prompt = extract_anthropic_prompt_and_response(example["chosen"])
+        return {
+            "prompt":   prompt,
+            "chosen":   example["chosen"][len(prompt) :],
+            "rejected": example["rejected"][len(prompt) :],
+        }
+
+
+@dataclass
+class PKUSyntheticRDP(RawDatasetPreprocessor):
+    # path: Optional[str] = "./data/pku_better_sft.hf"
+    # path: Optional[str] = "./data/pku_better_rejected_llama3init.hf"
+    path: Optional[str] = "./data/pku_safe_rejected_llama3init.hf"
+
+    # def __post_init__(self):
+    #     assert self.prompt_template == "\n\nHuman: {prompt}\n\nAssistant:"
+
+    def _get_raw_dataset(self, split):
+        if split == "train":
+            return load_from_disk(self.path)['train'].train_test_split(test_size=0.1, seed=0)["train"]
+        elif split == "validation":
+            return load_from_disk(self.path)['train'].train_test_split(test_size=0.1, seed=0)["test"]
+        elif split == "test":
+            return load_from_disk(self.path)['test']
+        else:
+            raise NotImplementedError
+
+    def _dataset_to_preference_formatter(self, example) -> Dict[str, str]:
+        example["chosen"]   = preprocess_anthropic_prompt_and_response(example["chosen"])
+        example["rejected"] = preprocess_anthropic_prompt_and_response(example["rejected"])
+        prompt = extract_anthropic_prompt_and_response(example["chosen"])
+        return {
+            "prompt":   prompt,
+            "chosen":   example["chosen"][len(prompt) :],
+            "rejected": example["rejected"][len(prompt) :],
+        }
+        
+@dataclass
+class PKUSyntheticRewardRDP(RawDatasetPreprocessor):
+    # path: Optional[str] = "./data/pku_better_sft.hf"
+    # path: Optional[str] = "./data/pku_better_rejected_llama3init.hf"
+    path: Optional[str] = "./data/pku_safe_with_reward_split.hf"
+
+    # def __post_init__(self):
+    #     assert self.prompt_template == "\n\nHuman: {prompt}\n\nAssistant:"
+
+    def _get_raw_dataset(self, split):
+        if split == "train":
+            return load_from_disk(self.path)['train'].train_test_split(test_size=0.1, seed=0)["train"]
+        elif split == "validation":
+            return load_from_disk(self.path)['train'].train_test_split(test_size=0.1, seed=0)["test"]
+        elif split == "test":
+            return load_from_disk(self.path)['test']
+        else:
+            raise NotImplementedError
+
+    def _dataset_to_preference_formatter(self, example) -> Dict[str, str]:
+        example["text"]   = preprocess_anthropic_prompt_and_response(example["text"])
+        prompt = extract_anthropic_prompt_and_response(example["text"])
+        return {
+            "prompt":   prompt,
+            "chosen":   example["text"][len(prompt) :],
+            "rejected": example["text"][len(prompt) :],
+        }
+
+if __name__ == "__main__":
+    # train_dataset      = HhRlhfRDP(num_proc=1).get_preference_dataset(split="train")
+    # validation_dataset = HhRlhfRDP(num_proc=1).get_preference_dataset(split="validation")
+    # test_dataset       = HhRlhfRDP(num_proc=1).get_preference_dataset(split="test")
+    sft_dataset = PKUSyntheticRDP(num_proc=1).get_sft_dataset(split='train')
---
+++ sft_train.py
@@ -0,0 +1,150 @@
+import os
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+from accelerate import Accelerator
+import torch
+from datasets import load_dataset
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, HfArgumentParser, TrainingArguments, AutoTokenizer
+from trl import SFTTrainer, set_seed, DataCollatorForCompletionOnlyLM
+from peft import LoraConfig
+import numpy as np
+import pandas as pd
+from data_utils import PKUSyntheticRDP
+tqdm.pandas()
+
+
+# define paths for two datasets
+hhrlhf_dataset_path = 'Anthropic/hh-rlhf'
+summary_dataset_path = 'openai/summarize_from_feedback'
+
+
+@dataclass
+class ScriptArguments:
+    log_with: Optional[str] = field(default='wandb', metadata={"help": "use 'wandb' to log with wandb"})
+    save_directory: Optional[str] = field(default='./logs_sft/')
+    learning_rate: Optional[float] = field(default=1.4e-4, metadata={"help": "the learning rate"})
+    batch_size: Optional[int] = field(default=8, metadata={"help": "the batch size"})
+    max_length: Optional[int] = field(default=512, metadata={"help": "max length"})
+    gradient_accumulation_steps: Optional[int] = field(default=1, metadata={"help": "the number of gradient accumulation steps"})
+    load_in_8bit: Optional[bool] = field(default=True, metadata={"help": "loading model in 8 bit or bfloat16"})
+    wandb_name: Optional[str] = field(default='pku_better_sft_llama3_3b_instruct', metadata={"help": "Name for this experiment"})
+    exp_type: Optional[str] = field(default='assistant', metadata={"help": "exp type, 'summary' or 'assistant' "})
+    base_model_name: Optional[str] = field(default="meta-llama/Llama-3.2-3B-Instruct", metadata={"help": "local path to the base model or the huggingface id"})
+    # padding_side: Optional[str] = field(default="right", metadata={"help": "tokenizer padding side"})
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+exp_type = script_args.exp_type
+base_model_name = script_args.base_model_name
+tokenier_name = base_model_name # we use the same tokenizer for the base model
+now = datetime.now()
+wandb_name = f"{script_args.wandb_name}_{now.strftime('%m%d%H%M')}"
+print('base model: ', base_model_name)
+os.makedirs(os.path.join(script_args.save_directory, wandb_name), exist_ok=True)
+
+training_args = TrainingArguments(
+        max_steps=30000,  
+        num_train_epochs=2,
+        output_dir=os.path.join(script_args.save_directory, wandb_name),
+        dataloader_drop_last=True,
+        eval_steps=5000,
+        evaluation_strategy='steps',
+        save_steps=5000,
+        logging_steps=10,
+        logging_strategy='steps',
+        save_strategy='steps',
+        per_device_train_batch_size=script_args.batch_size,
+        per_device_eval_batch_size=script_args.batch_size,
+        learning_rate=script_args.learning_rate,
+        lr_scheduler_type="linear",
+        warmup_steps=0,
+        gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+        gradient_checkpointing=False,
+        weight_decay=0.01,
+        run_name=wandb_name,
+        report_to=script_args.log_with,
+        ddp_find_unused_parameters=False,
+    )
+
+process_id = Accelerator().local_process_index 
+gpu_id = process_id 
+print('process: {}, model gpu id: {}'.format(process_id, gpu_id))
+
+
+# set seed before initializing value head for deterministic eval
+set_seed(8888)
+current_device = Accelerator().local_process_index
+print(current_device)
+
+lora_config = LoraConfig(
+    r=64, 
+    lora_alpha=128, 
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(tokenier_name)
+tokenizer.pad_token_id = tokenizer.eos_token_id
+tokenizer.padding_side = "right"
+
+if script_args.load_in_8bit:
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model_name, 
+        load_in_8bit=True, 
+        device_map=gpu_id, 
+    )
+else:
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model_name, 
+        torch_dtype=torch.bfloat16,
+        device_map=gpu_id, 
+    )
+model.config.pad_token_id = tokenizer.pad_token_id
+
+# if exp_type == 'assistant':
+#     dataset = build_dataset(hhrlhf_dataset_path, tokenizer, split='train') 
+#     valid_dataset = build_dataset(hhrlhf_dataset_path, tokenizer, split='test') 
+#     collator = DataCollatorForCompletionOnlyLM(response_template=Instructions.response_split, tokenizer=tokenizer, mlm=False)
+# else:
+#     dataset = build_dataset_summary(summary_dataset_path, tokenizer, split='train')
+#     collator = DataCollatorForCompletionOnlyLM(response_template=Instructions_summary.response_split, tokenizer=tokenizer, mlm=False)
+
+rdp = PKUSyntheticRDP(
+    prompt_template="\n\nHuman:\n{raw_prompt}\n\nAssistant:\n",
+    sanity_check=False,
+)
+train_dataset = rdp.get_sft_dataset(split="train", chosen_only=True)
+valid_dataset  = rdp.get_sft_dataset(split="validation", chosen_only=True)
+# train_dataset = PKUSyntheticRDP().get_sft_dataset(split="train")
+# valid_dataset = PKUSyntheticRDP().get_sft_dataset(split="validation")
+train_dataset = train_dataset.shuffle()
+
+print(f"Size of the train set: {len(train_dataset)}")
+
+trainer = SFTTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=valid_dataset,
+    tokenizer=tokenizer,
+    peft_config=lora_config,
+    packing=False,
+    dataset_text_field="prompt",
+    max_seq_length=script_args.max_length
+)
+
+print("Training........")
+if Accelerator().is_local_main_process and script_args.load_in_8bit:
+    trainer.model.print_trainable_parameters()
+trainer.train()
+
+if process_id == 0:
+    print("Saving last checkpoint of the model")
+    save_path = os.path.join(script_args.save_directory, wandb_name, 'model')
+    trainer.model.save_pretrained(save_path)
+    tokenizer.save_pretrained(save_path)
+    
+
